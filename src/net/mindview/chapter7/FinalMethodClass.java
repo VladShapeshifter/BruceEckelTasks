1095068aee5d03355884c9bba5eea6f58fbda72f
VladShapeshifter <vladislav.borsch@gmail.com>
2015-05-18 20:49:47 +0300

    done

---
+++ src/net/mindview/chapter7/FinalMethodClass.java
@@ -0,0 +1,7 @@
+package net.mindview.chapter7;
+
+public class FinalMethodClass {
+    final void method() {
+        System.out.println("final method()");
+    }
+}
---
+++ src/net/mindview/chapter7/FinalMethodClassTest.java
@@ -0,0 +1,8 @@
+package net.mindview.chapter7;
+
+public class FinalMethodClassTest extends FinalMethodClass {
+/*    @Override
+    final void method() {
+
+    }*/
+}
