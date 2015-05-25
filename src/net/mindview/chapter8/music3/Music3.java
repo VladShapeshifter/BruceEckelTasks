60b38f731b0ba088e19e2656ba5c28683a7b8daa
VladShapeshifter <vladislav.borsch@gmail.com>
2015-05-25 20:17:07 +0300

    add Random instruments generation

--- src/net/mindview/chapter8/music3/Music3.java
+++ src/net/mindview/chapter8/music3/Music3.java
@@ -1,5 +1,7 @@
 package net.mindview.chapter8.music3;
 
+import java.util.Random;
+
 public class Music3 {
     public static void tune(Instrument i) {
         i.play(Note.MIDDLE_C);
@@ -10,9 +12,23 @@ public class Music3 {
             tune(i);
         }
     }
+    private static Random rand = new Random();
+    public static Instrument next() {
+        switch (rand.nextInt(6)) {
+            default:
+            case 0: return new Wind();
+            case 1: return new Percussion();
+            case 2: return new Stringer();
+            case 3: return new Brass();
+            case 4: return new Woodwind();
+            case 5: return new Piano();
+        }
+    }
+
+
 
     public static void main(String[] args) {
-        Instrument[] orchestra = {
+        /*Instrument[] orchestra = {
                 new Wind(),
                 new Percussion(),
                 new Stringer(),
@@ -20,6 +36,14 @@ public class Music3 {
                 new Woodwind(),
                 new Piano()
         };
-        tuneAll(orchestra);
+        tuneAll(orchestra);*/
+        Instrument[] instr = new Instrument[6];
+        for (int i = 0; i < instr.length; i++) {
+            instr[i] = next();
+        }
+        for (Instrument i : instr) {
+            i.play(Note.B_FLAT);
+        }
+
     }
 }
