5e83620f9caa7c716398b8b5494e7b90abe32b14
Vladislav <vladislav.borsch@gmail.com>
2015-06-01 22:00:01 +0300

    done

--- src/net/mindview/chapter9/AbstractClass.java
+++ src/net/mindview/chapter9/AbstractClass.java
@@ -1,7 +1,5 @@
 package net.mindview.chapter9;
 
 public abstract class AbstractClass {
-    public static void main(String[] args) {
-//        AbstractClass abstractClass = new AbstractClass();
-    }
+    abstract void method1();
 }
--- src/net/mindview/chapter9/InheritedClassTwo.java
+++ src/net/mindview/chapter9/InheritedClassTwo.java
@@ -1,4 +1,17 @@
 package net.mindview.chapter9;
 
-public class InheritedClassTwo {
+public class InheritedClassTwo extends AbstractClass {
+    void method1() {
+        System.out.println("method1");
+    }
+    static void method2(AbstractClass ac) {
+//        ((InheritedClassTwo)ac).method1();
+        ac.method1();
+    }
+
+    public static void main(String[] args) {
+        InheritedClassTwo ic2 = null;
+        AbstractClass ac = new InheritedClassTwo();
+        ic2.method2(ac);
+    }
 }
