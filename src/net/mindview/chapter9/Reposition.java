e4bb535e392b8dbec899578bf06f9682102ae367
VladShapeshifter <vladislav.borsch@gmail.com>
2015-06-04 22:22:40 +0300

    done

--- src/net/mindview/chapter9/Reposition.java
+++ src/net/mindview/chapter9/Reposition.java
@@ -9,6 +9,17 @@ public class Reposition implements Processor {
     @Override
     public Object process(Object input) {
         char[] chars =  ((String)input).toCharArray();
-        return null;
+        char[] newChars = new char[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            if (i < chars.length - 1) {
+                newChars[i++] = chars[i];
+                newChars[i--] = chars[i];
+            }
+        }
+        return newChars.toString();
+    }
+
+    public static void main(String[] args) {
+        Apply.process(new Reposition(), "abc");
     }
 }
