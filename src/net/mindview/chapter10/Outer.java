523eceab09faa6036d09d6b4ab1b640dc72b502d
VladShapeshifter <vladislav.borsch@gmail.com>
2015-06-09 22:00:39 +0300

    update

--- src/net/mindview/chapter10/Outer.java
+++ src/net/mindview/chapter10/Outer.java
@@ -1,15 +1,23 @@
 package net.mindview.chapter10;
 
 public class Outer {
+    private String s;
+    Outer() {
+        s = "Outer constructor initialisation in Inner class using";
+    }
     Inner classref() {
         return new Inner();
     }
     class Inner {
-
+        @Override
+        public String toString() {
+            return s;
+        }
     }
 
     public static void main(String[] args) {
         Outer outer = new Outer();
         Inner inner = outer.classref();
+        System.out.println(inner);
     }
 }
