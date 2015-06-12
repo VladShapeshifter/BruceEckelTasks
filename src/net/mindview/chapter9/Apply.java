e99efa516b0eb2676a595b3b98c8f3b819a36fba
VladShapeshifter <vladislav.borsch@gmail.com>
2015-06-12 20:31:18 +0300

    update

--- src/net/mindview/chapter9/Apply.java
+++ src/net/mindview/chapter9/Apply.java
@@ -5,4 +5,26 @@ public class Apply {
         System.out.println("Using processor " + p.name());
         System.out.println(p.process(s));
     }
+
+    public static void main(String[] args) {
+        Apply.process(new Processor() {
+            @Override
+            public String name() {
+                return getClass().getSimpleName();
+            }
+
+            @Override
+            public Object process(Object input) {
+                char[] chars =  ((String)input).toCharArray();
+                char[] newChars = new char[chars.length];
+                for (int i = 0; i < chars.length; i++) {
+                    if (i < chars.length - 1) {
+                        newChars[i++] = chars[i];
+                        newChars[i--] = chars[i];
+                    }
+                }
+                return newChars.toString();
+            }
+        }, "abc");
+    }
 }
