ff697c0496354f0eb155e29f2230403e7b047be4
VladShapeshifter <vladislav.borsch@gmail.com>
2015-06-12 21:17:36 +0300

    done

---
+++ src/net/mindview/chapter10/NestedClass.java
@@ -0,0 +1,9 @@
+package net.mindview.chapter10;
+
+public class NestedClass {
+    static class Inside1 {
+        static class Inside2 {
+
+        }
+    }
+}
---
+++ src/net/mindview/chapter10/NestedClassInside.java
@@ -0,0 +1,11 @@
+package net.mindview.chapter10;
+
+public class NestedClassInside {
+    static class Inner {
+
+    }
+
+    public static void main(String[] args) {
+        Inner inner = new Inner();
+    }
+}
