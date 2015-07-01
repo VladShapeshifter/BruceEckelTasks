e3e17f3b7578067dc86d994de65699d9453156cf
VladShapeshifter <vladislav.borsch@gmail.com>
2015-07-01 21:24:34 +0300

    done

--- src/net/mindview/chapter11/ListFeatures.java
+++ src/net/mindview/chapter11/ListFeatures.java
@@ -1,8 +1,6 @@
 package net.mindview.chapter11;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class ListFeatures {
     public static void main(String[] args) {
@@ -16,5 +14,55 @@ public class ListFeatures {
         numb.add(9);
         numb.add(5);
         System.out.println("1: " + numb);
+        int i = 6;
+        numb.add(6);
+        System.out.println("2: " + numb);
+        System.out.println("3: " + numb.contains(i));
+        numb.remove(i);
+        int y = numb.get(2);
+        System.out.println("4: " + y + " " + numb.indexOf(y));
+        int j = new Integer(9);
+        System.out.println("5: " + numb.indexOf(j));
+//        System.out.println("6: " + numb.remove(j));
+//        System.out.println("7: " + numb.remove(y));
+        System.out.println("8: " + numb);
+        numb.add(3, new Integer(4));
+        System.out.println("9: " + numb);
+        List<Integer> sub = numb.subList(1, 4);
+        System.out.println("Sublist is: " + sub);
+        System.out.println("10: " + numb.containsAll(sub));
+        Collections.sort(sub);
+        System.out.println("After sort: " + sub);
+        System.out.println("11: " + numb.containsAll(sub));
+        Collections.shuffle(sub, rand);
+        System.out.println("After shuffle: " + sub);
+        System.out.println("12: " + numb.containsAll(sub));
+        List<Integer> copy = new ArrayList<>(numb);
+        sub = Arrays.asList(numb.get(1), numb.get(4));
+        System.out.println("sub: " + sub);
+        copy.retainAll(sub);
+        System.out.println("13: " + sub);
+        copy = new ArrayList<>(numb);
+        copy.remove(2);
+        System.out.println("14: " + copy);
+        copy.removeAll(sub);
+        System.out.println("15: " + copy);
+        copy.set(1, new Integer(9));
+        System.out.println("16: " + copy);
+        copy.addAll(2, sub);
+        System.out.println("17: " + copy);
+        System.out.println("18: " + numb.isEmpty());
+        numb.clear();
+        System.out.println("19: " + numb);
+        System.out.println("20: " + numb.isEmpty());
+        numb.add(1);
+        numb.add(2);
+        numb.add(3);
+        numb.add(4);
+        System.out.println("21: " + numb);
+        Object[] o = numb.toArray();
+        System.out.println("22: " + o[3]);
+        Integer[] integers = numb.toArray(new Integer[0]);
+//        System.out.println("23: " + integers[3].id()));
     }
 }
