d424662b2562e1411b5fadcb2a77a39d5662e4ec
VladShapeshifter <vladislav.borsch@gmail.com>
2015-07-02 22:29:58 +0300

    Iterator realisation

--- src/net/mindview/chapter8/animals/RodentCage.java
+++ src/net/mindview/chapter8/animals/RodentCage.java
@@ -1,15 +1,27 @@
 package net.mindview.chapter8.animals;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class RodentCage {
     public static void main(String[] args) {
         Hamster hamster = new Hamster();
-        Rodent[] rodents = new Rodent[3];
+        ArrayList<Rodent> rodents1 = new ArrayList<>();
+        rodents1.add(new Mouse(hamster));
+        rodents1.add(new Mouse(hamster));
+        rodents1.add(new Beaver(hamster));
+        Iterator<Rodent> it = rodents1.iterator();
+        while (it.hasNext()) {
+            it.next().gnaw(Meal.WOOD);
+            System.out.println(it);
+        }
+        /*Rodent[] rodents = new Rodent[3];
         rodents[0] = new Mouse(hamster);
         rodents[1] = new Mouse(hamster);
         rodents[2] = new Beaver(hamster);
         for (Rodent r : rodents) {
             r.gnaw(Meal.WOOD);
             System.out.println(r.info());
-        }
+        }*/
     }
 }
