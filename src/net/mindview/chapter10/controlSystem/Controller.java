345b7c7e6e59cbb20c2be7f95d1763abd8db91ec
Vladislav <vladislav.borsch@gmail.com>
2015-07-05 23:55:13 +0300

    almost done with Iterator realisation

--- src/net/mindview/chapter10/controlSystem/Controller.java
+++ src/net/mindview/chapter10/controlSystem/Controller.java
@@ -1,24 +1,36 @@
 package net.mindview.chapter10.controlSystem;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Controller {
-    private List<Event> eventList = new ArrayList<Event>();
+    private List<Event> eventList = new LinkedList<>();
 
     public void addEvent(Event c) {
         eventList.add(c);
     }
 
     public void run() {
-        while (eventList.size() > 0) {
-            for (Event e : new ArrayList<Event>(eventList)) {
+        LinkedList<Event> events = new LinkedList<>(eventList);
+        Iterator<Event> it = events.iterator();
+        while (it.hasNext()) {
+//            if (it.next().ready()) {
+                Event e = it.next();
+                System.out.println(e);
+                e.action();
+                it.remove();
+//            }
+        }
+
+        /*while (eventList.size() > 0) {
+            for (Event e : new LinkedList<>(eventList)) {
                 if (e.ready()) {
                     System.out.println(e);
                     e.action();
                     eventList.remove(e);
                 }
             }
-        }
+        }*/
     }
 }
