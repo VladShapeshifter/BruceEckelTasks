72272ba5218a2471478217e35bbcedc52c2b0bcb
VladShapeshifter <vladislav.borsch@gmail.com>
2015-07-08 21:00:44 +0300

    done with WordsStorage class realisation instead Map<String, Integer>

--- src/net/mindview/chapter11/WordsOccurrence.java
+++ src/net/mindview/chapter11/WordsOccurrence.java
@@ -4,20 +4,36 @@ import java.util.*;
 
 public class WordsOccurrence {
     void count(String sentence, String words) {
-        Map<String, Integer> wordsFreq = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+//        Map<String, Integer> wordsFreq = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+//        Set<String> splitedWordsSet = new HashSet<>();
+//        Collections.addAll(splitedWordsSet, words.split(" "));
         String[] splitedWords = words.split(" ");
-        for (int i = 0; i < splitedWords.length; i++) {
-            wordsFreq.put(splitedWords[i], 0);
-        }
+//        Iterator<String> it = splitedWordsSet.iterator();
+//        while (it.hasNext()) {
+//            wordsFreq.put(it.next(), 0);
+//        }
+        Set<WordsStorage> wordsStorage = new HashSet<>();
 
+        for (int i = 0; i < splitedWords.length; i++) {
+//            wordsFreq.put(splitedWords[i], 0);
+            wordsStorage.add(new WordsStorage(splitedWords[i], 0));
+        }
         ArrayList<String> inputWords = new ArrayList<>(Arrays.asList(sentence.split(" ")));
         for (String word : inputWords) {
-            if (wordsFreq.containsKey(word)) {
+            Iterator<WordsStorage> it = wordsStorage.iterator();
+            while (it.hasNext()) {
+                WordsStorage wordsStorage1 = it.next();
+                if (wordsStorage1.word.equals(word)) {
+                    wordsStorage1.freq++;
+                }
+            }
+
+            /*if (wordsFreq.containsKey(word)) {
                 Integer freq = wordsFreq.get(word);
                 wordsFreq.put(word, ++freq);
-            }
+            }*/
         }
-        System.out.println(wordsFreq);
+        System.out.println(wordsStorage);
     }
 
     public static void main(String[] args) {
