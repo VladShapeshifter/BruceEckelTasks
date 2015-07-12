b19188a1aaec47b8fc271fb6140dd121e2123a15
Vladislav <vladislav.borsch@gmail.com>
2015-07-12 21:24:39 +0300

    done

--- src/net/mindview/chapter8/shape/RandomShapeGenerator.java
+++ src/net/mindview/chapter8/shape/RandomShapeGenerator.java
@@ -1,16 +1,56 @@
 package net.mindview.chapter8.shape;
 
+import java.util.Iterator;
 import java.util.Random;
 
-public class RandomShapeGenerator {
+public class RandomShapeGenerator implements Iterable<Shape> {
     private Random rand = new Random();
-    public Shape next() {
-        switch(rand.nextInt(4)) {
+
+    /*public Shape next() {
+        switch (rand.nextInt(4)) {
             default:
-            case 0: return new Circle();
-            case 1: return new Square();
-            case 2: return new Triangle();
-            case 3: return new Rhombus();
+            case 0:
+                return new Circle();
+            case 1:
+                return new Square();
+            case 2:
+                return new Triangle();
+            case 3:
+                return new Rhombus();
         }
+    }*/
+
+    private int shapeCount;
+
+    RandomShapeGenerator(int i) {
+        shapeCount = i;
+    }
+
+    @Override
+    public Iterator<Shape> iterator() {
+        return new Iterator<Shape>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < shapeCount;
+            }
+
+            @Override
+            public Shape next() {
+                index++;
+                switch (rand.nextInt(4)) {
+                    default:
+                    case 0:
+                        return new Circle();
+                    case 1:
+                        return new Square();
+                    case 2:
+                        return new Triangle();
+                    case 3:
+                        return new Rhombus();
+                }
+            }
+        };
     }
 }
--- src/net/mindview/chapter8/shape/Shapes.java
+++ src/net/mindview/chapter8/shape/Shapes.java
@@ -1,15 +1,19 @@
 package net.mindview.chapter8.shape;
 
 public class Shapes {
-    private static RandomShapeGenerator gen = new RandomShapeGenerator();
+    private static RandomShapeGenerator gen = new RandomShapeGenerator(4);
 
     public static void main(String[] args) {
-        Shape[] s = new Shape[9];
+        /*Shape[] s = new Shape[9];
         for (int i = 0; i < s.length; i++) {
             s[i] = gen.next();
         }
         for (Shape shp : s) {
             shp.paint();
+        }*/
+        for (Shape sh : gen) {
+            sh.paint();
         }
+
     }
 }
