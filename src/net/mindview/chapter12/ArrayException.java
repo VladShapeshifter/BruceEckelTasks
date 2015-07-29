0f5264c3e93d47be1ddacfa2f48c42677438732a
VladShapeshifter <vladislav.borsch@gmail.com>
2015-07-29 21:04:54 +0300

    add information about exception

--- src/net/mindview/chapter12/ArrayException.java
+++ src/net/mindview/chapter12/ArrayException.java
@@ -1,6 +1,16 @@
 package net.mindview.chapter12;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
 public class ArrayException {
+    private static Logger logger = Logger.getLogger("No such element in the array");
+    static void logException(Exception e) {
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5};
         for (int i = 0; i < 6; i++) {
@@ -8,6 +18,7 @@ public class ArrayException {
                 System.out.println(nums[i]);
             } catch (ArrayIndexOutOfBoundsException ae) {
                 System.out.println("No such element in the array");
+                logException(ae);
             }
 
         }
