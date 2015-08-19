00352bd0262d8d7731a10c884bdb804b4844b9db
Vladislav <vladislav.borsch@gmail.com>
2015-08-19 11:44:12 +0300

    done with using StringBuilder

--- src/net/mindview/chapter13/SprinklerSystem.java
+++ src/net/mindview/chapter13/SprinklerSystem.java
@@ -8,13 +8,35 @@ public class SprinklerSystem {
 
     @Override
     public String toString() {
-        return
-            "valve1 = " + valve1 + " " +
+        StringBuilder result = new StringBuilder();
+        result.append("valve1 = ");
+        result.append(valve1);
+        result.append(" ");
+        result.append("valve2 = ");
+        result.append(valve2);
+        result.append(" ");
+        result.append("valve3 = ");
+        result.append(valve3);
+        result.append(" ");
+        result.append("valve4 = ");
+        result.append(valve4);
+        result.append("\n");
+        result.append("i = ");
+        result.append(i);
+        result.append(" ");
+        result.append("f = ");
+        result.append(f);
+        result.append(" ");
+        result.append("source = ");
+        result.append(source);
+        return result.toString();
+
+            /*"valve1 = " + valve1 + " " +
             "valve2 = " + valve2 + " " +
             "valve3 = " + valve3 + " " +
             "valve4 = " + valve4 + "\n" +
             "i = " + i + " " + "f = " + f + " " +
-            "source = " + source;
+            "source = " + source;*/
     }
 
     public static void main(String[] args) {
