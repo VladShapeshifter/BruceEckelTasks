1d7490955fbb384a252e21f823f5ec40a5f07f58
VladShapeshifter <vladislav.borsch@gmail.com>
2015-08-21 20:36:49 +0300

    done

--- src/net/mindview/chapter13/Receipt.java
+++ src/net/mindview/chapter13/Receipt.java
@@ -3,23 +3,23 @@ package net.mindview.chapter13;
 import java.util.Formatter;
 
 public class Receipt {
-    private int width10 = 10;
     private int width5 = 5;
-    private int width15 = 15;
+    private int width10 = width5*2;
+    private int width15 = width5*3;
     private double total = 0;
     private Formatter f = new Formatter(System.out);
     public void printTitle() {
-        f.format("%-15s %" + width5 + "s %" + width10 + "s\n", "Item", "Qty", "Price");
-        f.format("%-15s %" + width5 + "s %" + width10 + "s \n", "----", "---", "-----");
+        f.format("%-" + width15 + "s %" + width5 + "s %" + width10 + "s\n", "Item", "Qty", "Price");
+        f.format("%-" + width15 + "s %" + width5 + "s %" + width10 + "s \n", "----", "---", "-----");
     }
     public void print(String name, int qty, double price) {
-        f.format("%-15.15s %5d %10.2f\n", name, qty, price);
+        f.format("%-" + width15 + ".15s %" + width5 + "d %" + width10 + ".2f\n", name, qty, price);
         total += price;
     }
     public void printTotal() {
-        f.format("%-15s %5s %10.2f\n", "Tax", "", total*0.06);
-        f.format("%-15s %5s %10s\n", "", "", "-----");
-        f.format("%-15s %5s %10.2f\n", "Total", "", total*1.06);
+        f.format("%-" + width15 + "s %" + width5 + "s %" + width10 + ".2f\n", "Tax", "", total*0.06);
+        f.format("%-" + width15 + "s %" + width5 + "s %" + width10 + "s\n", "", "", "-----");
+        f.format("%-" + width15 + "s %" + width5 + "s %" + width10 + ".2f\n", "Total", "", total*1.06);
     }
 
     public static void main(String[] args) {
