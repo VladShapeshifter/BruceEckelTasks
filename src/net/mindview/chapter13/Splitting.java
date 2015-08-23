05cfe76aa2c58a9258a13699e362d203b541d8ba
Vladislav <vladislav.borsch@gmail.com>
2015-08-23 13:13:11 +0300

    done next lesson

--- src/net/mindview/chapter13/Splitting.java
+++ src/net/mindview/chapter13/Splitting.java
@@ -9,5 +9,16 @@ public class Splitting {
     public static void main(String[] args) {
         String[] splited = knights.split("the|you");
         System.out.println(Arrays.toString(splited));
+
+        String knights2 = "";
+        char[] chars = knights.toCharArray();
+        for (char c : chars) {
+            if (Character.toString(c).matches("a|e|i|o|u|A|E|I|O|U")) {
+                knights2 += "_";
+            } else {
+                knights2 += Character.toString(c);
+            }
+        }
+        System.out.println(knights2);
     }
 }
