487abc9d4050bea0265fce547c2425c63b1e05c6
VladShapeshifter <vladislav.borsch@gmail.com>
2015-08-25 22:14:25 +0300

    done with another lesson

--- src/net/mindview/chapter13/Matchings.java
+++ src/net/mindview/chapter13/Matchings.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 public class Matchings {
     public static void main(String[] args) {
         String s = "Java now has regular expressions";
+        String ss = "Arline ate eight apples and one orange while Anita hadn't any";
         String expr1 = "^Java";
         String expr2 = "\\Breg.*";
         String expr3 = "n.w\\s+h(a|i)s";
@@ -16,8 +17,10 @@ public class Matchings {
         String expr8 = "s{1}.";
         String expr9 = "s{0,3}";
 
-        Pattern regex = Pattern.compile(expr9);
-        Matcher matcher = regex.matcher(s);
+        String someExpr = "(?i)((^[aeiou])|(\\s+[aeiou]))\\w+?[aeiou]\\b";
+
+        Pattern regex = Pattern.compile(someExpr);
+        Matcher matcher = regex.matcher(ss);
         boolean b = matcher.find();
         System.out.println(b);
     }
