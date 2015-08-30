7bdbb740ed6202fd7f3a3b67743beee6656c489f
Vladislav <vladislav.borsch@gmail.com>
2015-08-30 14:25:09 +0300

    done with search in all files in current folder

--- src/net/mindview/chapter13/JGrep.java
+++ src/net/mindview/chapter13/JGrep.java
@@ -2,6 +2,8 @@ package net.mindview.chapter13;
 
 import net.mindview.util.TextFile;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,17 +13,35 @@ public class JGrep {
             System.out.println("Usage: java JGrep file regex");
             System.exit(0);
         }
-        /*Pattern compile = Pattern.compile("\"");
-        Matcher matcher = compile.matcher(args[2]);
-        String s = matcher.replaceAll("");
-        System.out.println(s);*/
         Pattern p = Pattern.compile(args[1]);
         int index = 0;
         Matcher m = p.matcher("");
-        for (String line : new TextFile(args[0])) {
-            m.reset(line);
-            while (m.find()) {
-                System.out.println(index++ + ": " + m.group() + ": " + m.start());
+
+        Pattern pp = Pattern.compile("(?m).+\\.java$");
+        Matcher mm = pp.matcher(args[0]);
+        if (mm.find()) {
+            for (String s : new TextFile(args[0])) {
+                m.reset(s);
+                while (m.find()) {
+                    System.out.println(index++ + ": " + m.group() + ": " + m.start());
+                }
+            }
+        } else {
+            File[] files = new File(args[0]).listFiles();
+            String allFilesFromPath = Arrays.toString(files);
+            String[] splitedPaths = allFilesFromPath.split(", ");
+            for (String path : splitedPaths) {
+                String normalPath = path.replaceAll("\\[", "").replaceAll("\\]", "");
+                Pattern ppp = Pattern.compile("(?m).+\\.java$");
+                Matcher mmm = ppp.matcher(normalPath);
+                while (mmm.find()) {
+                    for (String s : new TextFile(normalPath)) {
+                        m.reset(s);
+                        while (m.find()) {
+                            System.out.println(index++ + ": " + m.group() + ": " + m.start());
+                        }
+                    }
+                }
             }
         }
     }
