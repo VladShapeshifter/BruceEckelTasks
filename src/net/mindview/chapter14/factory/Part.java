60ea9479559a8d6d140e866d35d6257eb6ecb7b0
Vladislav <vladislav.borsch@gmail.com>
2015-10-27 11:27:54 +0200

    done with .class and newInstance() realisation

--- src/net/mindview/chapter14/factory/Part.java
+++ src/net/mindview/chapter14/factory/Part.java
@@ -9,19 +9,23 @@ class Part {
     public String toString() {
         return getClass().getSimpleName();
     }
-    static List<Factory<? extends Part>> partFactories = new ArrayList<>();
+    static List<Class<? extends Part>> partFactories = new ArrayList<>();
     static {
-        partFactories.add(new FuelFilter.Factory());
-        partFactories.add(new AirFilter.Factory());
-        partFactories.add(new CabinAirFilter.Factory());
-        partFactories.add(new OilFilter.Factory());
-        partFactories.add(new FanBelt.Factory());
-        partFactories.add(new PowerSteeringBelt.Factory());
-        partFactories.add(new GeneratorBelt.Factory());
+        partFactories.add(FuelFilter.class);
+        partFactories.add(AirFilter.class);
+        partFactories.add(CabinAirFilter.class);
+        partFactories.add(OilFilter.class);
+        partFactories.add(FanBelt.class);
+        partFactories.add(PowerSteeringBelt.class);
+        partFactories.add(GeneratorBelt.class);
     }
     private static Random rand = new Random(47);
     public static Part createRandom() {
         int n = rand.nextInt(partFactories.size());
-        return partFactories.get(n).create();
+        try {
+            return partFactories.get(n).newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
