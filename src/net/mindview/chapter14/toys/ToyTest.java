eec3adf25e65f50de46f1224c170ea338104528d
Vladislav <vladislav.borsch@gmail.com>
2015-11-26 23:59:37 +0200

    create object with constructor (reflection)

--- src/net/mindview/chapter14/toys/ToyTest.java
+++ src/net/mindview/chapter14/toys/ToyTest.java
@@ -1,10 +1,14 @@
 package net.mindview.chapter14.toys;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class ToyTest {
     static void printInfo(Class cc) {
         System.out.println("Class name: " + cc.getName() + " is interface? [" + cc.isInterface() + "]");
         System.out.println("Simple name: " + cc.getSimpleName());
         System.out.println("Canonical name: " + cc.getCanonicalName());
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -21,7 +25,14 @@ public class ToyTest {
         }
         Class up = c.getSuperclass();
         Object obj = null;
-        try {
+        for (Constructor constructor : up.getDeclaredConstructors()) {
+            try {
+                obj = constructor.newInstance(1);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        /*try {
             obj = up.newInstance();
         } catch (InstantiationException e) {
             System.out.println("Can't create instance");
@@ -29,7 +40,7 @@ public class ToyTest {
         } catch (IllegalAccessException e) {
             System.out.println("Access denied");
             System.exit(1);
-        }
+        }*/
         printInfo(obj.getClass());
     }
 }
